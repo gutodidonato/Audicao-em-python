c8549bb9449948adc11f106ff6438a930ddea677
gutodidonato <rm99433@fiap.com.br>
2023-09-17 19:14:59 -0300

    finalizado

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 
 
 
-while True:
+for i in range(1):
     audicao = {}
     pessoas = int(input("Quantas pessoas vão participar? "))
     maior_valor = 0
@@ -13,13 +13,11 @@ while True:
     for k in range(pessoas):
         pessoa = input("Diga seu nome: ")
         valor = int((input("Quanto quer apostar? ")))
+        print("\n"*50)
         audicao[pessoa] = {}
         audicao[pessoa]['valor'] = valor
         if valor > maior_valor:
             maior_valor = valor
             pessoa_vencedora = pessoa
             
-
-    print(audicao)
-    print(maior_valor)
-    print(pessoa_vencedora)
+    print(f"O vencedor é {pessoa_vencedora} com uma aposta de ${maior_valor}")
